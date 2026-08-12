aca5a0988aa743d00133a660c290d60725fca12f
vinnizinolivei-spec <vinnizin.olivei@gmail.com>
2026-08-12 16:57:06 -0300

    aula1 - 12/08/26

---
+++ aula1/FichaPessoal.java
@@ -0,0 +1,22 @@
+public class FichaPessoal {
+
+    public static void main(String[]args) {
+        
+    String nome = "Vinni";
+    int idade = 16;
+    String cidade = "Joinville";
+    String curso = "Programação Orientada a Objetos";
+    String linguagem_de_programação_que_já_viu = "Python";
+    String hobbie = "Ler e escrever histórias";               
+    
+    System.out.println(nome);
+    System.out.println(idade);
+    System.out.println(curso);
+    System.out.println(linguagem_de_programação_que_já_viu);
+    System.out.println(hobbie);
+
+    int anoNascimento = 2010;
+    int anoAtual = 2026;
+    int idadeCalculada = anoAtual - anoNascimento;
+    }
+}
